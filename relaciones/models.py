899d6c827293cbf82c6b3a62cddca79e447f0f29
Pablo <pfampuerogonzalez@gmail.com>
2026-06-09 00:53:01 +0000

    demo

---
+++ relaciones/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+#relacion 1 a muchos
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
